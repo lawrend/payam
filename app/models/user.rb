42c9cc34c5d8c01ee61d94e9f94e64604367a6ba
lawrend <douglas_lawrence@mac.com>
2017-05-31 15:46:21 -0700

    updated views for login and edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,5 @@ class User < ApplicationRecord
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
   validates_uniqueness_of :username
-  attr_accessor :login
-
-  def login=(login)
-    @login = login
-  end
-
-  def login
-    @login || self.username || self.email
-  end
+  
 end
